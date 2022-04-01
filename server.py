7990217a40998487c5d2cebf3043b84c7644881a
Christian Ramirez <christian.r0602@gmail.com>
2022-04-01 11:33:18 -0500

    adding session

--- server.py
+++ server.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect
 
 app = Flask(__name__)
+app.secret_key = 'counter' #used to encrypt data
+
+
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    
+    if 'views' not in session:
+      session['views'] = 0
+    else:
+        session['views'] += 1
+    return render_template('index.html', count=session['views'])
+
+
+
+
 
 if (__name__) == ('__main__'):
     app.run(debug=True)
